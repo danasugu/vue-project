a1c6a9baef4ec7d92af628bad9b42b305ac3801d
Dana Sugu <dana.sugu@gmail.com>
2021-07-04 08:48:23 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1 class="text-4xl">Create Items</h1>
+<div>
+    <h1 class="text-4xl">Create Items</h1>
+  <div class="m-2 p-2 bg-gray-200">ds</div>
+  <input type="text" v-model="newItem" />
+</div>
+
 </template>
 
 <script>

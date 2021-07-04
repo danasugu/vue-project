b7faa3714daa85e97d8a36cb24aad0f131b1bfdd
Dana Sugu <dana.sugu@gmail.com>
2021-07-04 09:44:15 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-<!-- <div> -->
+<div>
    <h1 class="text-4xl">Create Items</h1>
   <div class="m-2 p-2 bg-gray-200">
   <input class="bg-blue-200 p-2 m-2" type="text" v-model="newItem" />
   <button @onclick="storeItem">Save</button>
   <button class="ml-5 p-2 bg-pink-300 rounded" @onclick="storeItem">Save</button>
   </div>
-<!-- </div> -->
+<div>
+  <ul>
+    <li v-for-key="item in items" key:item>{{ item }}</li>
+  </ul>
+</div>
+</div>
 </template>
 
 <script>

f920c9ce86982edb4a82737b32677af8bdb634e9
Dana Sugu <dana.sugu@gmail.com>
2021-07-04 08:19:27 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>{{title}}</h1>
+    <p v-for="item in items" :key="item">{{item}}</p>
   </div>
 </template>
 
@@ -9,7 +10,14 @@ export default {
   name: 'App',
   data(){
     return{
-      title:"this is the title"
+      title:"this is the title",
+      items:[
+        "this is item 1",
+        "this is item 2",
+        "this is item 3",
+        "this is item 4",
+        "this is item 5"
+      ],
       }
     }
   };

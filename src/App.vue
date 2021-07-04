a3a43242a4382b067697ac2b27e86de7427e4987
Dana Sugu <dana.sugu@gmail.com>
2021-07-04 08:01:04 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,18 @@
 <template>
   <div id="app">
-    
+    <h1></h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-  }
+  data(){
+    return{
+      title:"this is the title"
+      }
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>

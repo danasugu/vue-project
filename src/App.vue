93ec4dfd68d132d2b3f10f96d2f08833efa26712
Dana Sugu <dana.sugu@gmail.com>
2021-07-04 08:22:48 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,12 @@ export default {
     return{
       title:"this is the title",
       items:[
-        "this is item 1",
-        "this is item 2",
-        "this is item 3",
-        "this is item 4",
-        "this is item 5"
-      ],
+      {id:1, title:"this is item 1"},  
+      {id:2, title:"this is item 2"},  
+      {id:3, title:"this is item 3"},  
+      {id:4, title:"this is item 4"},  
+      {id:5, title:"this is item 5"},  
+         ],
       }
     }
   };
